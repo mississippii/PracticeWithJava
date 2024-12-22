7273cedc6f5b996c1cd66e35e293c78c42d77c78
T@nvir <u1604090@student.cuet.ac.bd>
2024-12-22 21:06:10 +0600

    fixed some issue

--- src/main/java/others/Others.java
+++ src/main/java/others/Others.java
@@ -2,9 +2,13 @@ package others;
 
 import others.generic.DynamicArray;
 
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 public class Others {
+<<<<<<< HEAD
     public static void main(String[] args){
         String str = "Hello World";
         String str2 = new String("Hello World");
@@ -27,5 +31,10 @@ public class Others {
             }
         }
         return prices;
+=======
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("/home/veer/Documents/Java/PracticeWithJava/src/main/resources/hello.txt");
+        System.out.println(path);
+>>>>>>> 498d744 (fixed some issue)
     }
 }
